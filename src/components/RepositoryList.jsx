ca73cb6514bab4e9a74582e0ff26f245b41a2ee9
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-02 13:58:35 -0300

    feat: adds properties to repository items

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,15 +1,21 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+const repository = {
+  name: 'unform',
+  description: 'Forms in React',
+  link: 'http://github.com/unform/unform'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
       </ul>
     </section>
   );
