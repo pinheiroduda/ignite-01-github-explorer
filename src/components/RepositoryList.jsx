97d3eb7df9ee39909093dcbd51059e80e2cf2d93
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-02 17:39:36 -0300

    feat: adds display of the api's consumed repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,12 +3,6 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'http://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -16,17 +10,17 @@ export function RepositoryList() {
     fetch('https://api.github.com/orgs/rocketseat/repos')
     .then(response => response.json()) //reposta convertida em json
     .then(data => setRepositories(data))
-  }, [])
+  }, []);
+
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
